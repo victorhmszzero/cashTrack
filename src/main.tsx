797ea0cd6bf460203c6caf562c35c73f1bf2d8dc
Victor Magalhaes <victorhms.vhms@gmail.com>
2026-04-18 10:17:19 -0300

    refactor: improve React root initialization and add type declarations

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,16 @@
 // src/main.tsx
-
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+
 import App from './App'
 import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+//                                                                                      //
+const rootElement = document.getElementById('root');
+
+if (!rootElement) throw new Error('Falha ao encontrar o elemento root');
+
+ReactDOM.createRoot(rootElement).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
